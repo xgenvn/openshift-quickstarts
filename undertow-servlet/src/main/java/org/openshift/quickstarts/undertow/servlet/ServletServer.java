7cbf0854e5b49784d04e4f034f65ea717587418e
Tu Nguyen <tu.nguyen@sioux.asia>
2018-07-20 14:47:30 +0700

    change hello text

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello World")
+                                    .addInitParam("message", "Hello Master. Wearing a very nice RedHat now!")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
